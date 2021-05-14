5c538467aa8fa22c4ae191517281710af8f8e09b
Trayshawn <traytwebb14@yahoo.com>
2021-05-14 16:28:30 -0500

    practice with functions, condtionals, and loops in python

---
+++ examples/functions_conditionals_loops.py
@@ -0,0 +1,40 @@
+number = 5 
+
+#conditionals
+if number > 10: 
+    print("This number is greater than 10.")
+else:
+    print("This number is less than 10.")
+
+if 10 < 100:
+    print("Yeah")
+
+# Fuction format
+# def fname(arg):
+#     pass -> used for functions that you're currently about to use
+
+def name_printer(name, value):
+    print(name, value)
+
+name_printer("Malcolm", 123)
+
+# for loops
+
+# for i in [1,2,3,4,5]:
+#     print("VALUE:", i)
+
+# for i in range(20):
+#     print(i)
+
+# While loops 
+
+x = 10 
+while x > 0:
+    print("x is still greater than 0")
+    x = x -1 
+
+
+
+
+
+
