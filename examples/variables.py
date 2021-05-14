afee82bcbf3d62f1309031ede397d097826cfd18
Trayshawn <traytwebb14@yahoo.com>
2021-05-13 20:08:19 -0500

    added variables.py twith variable practice in python

---
+++ examples/variables.py
@@ -0,0 +1,17 @@
+first_name = "Kwame"
+age = 29
+last_name = "Nkrumah"
+
+list = [1,2,3] #lists are like arrays in JS & Ruby
+nested_list = [[1,2,3],['a','b','c'],[True,False]] #use caps for booleans values
+
+full_name = first_name + " " + last_name
+
+x, y, z = 1,2,3 #simpliar to destructing in JS with an array
+
+
+
+print(full_name)
+print(list[1])
+print(nested_list[1][2])
+print(z)
