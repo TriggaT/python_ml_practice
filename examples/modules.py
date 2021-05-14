7392dcfa58ab196898bd156b27181ebeb73775a9
Trayshawn <traytwebb14@yahoo.com>
2021-05-14 17:21:59 -0500

    added file with math module practice

---
+++ examples/modules.py
@@ -0,0 +1,10 @@
+# Modules are similar to frameworks in other languages 
+import math # you can import modules 
+from math import sqrt # and import specific functions from modules
+
+x = math.pow(3,3)
+print(x)
+
+y = sqrt(29)
+print(y)
+
