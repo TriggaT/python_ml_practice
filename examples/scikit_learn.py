bf5599b4a3b6d66c702df2edaee903436f7430e0
Trayshawn <traytwebb14@yahoo.com>
2021-05-22 19:18:24 -0500

    imported train_test_split and split features and labels into training and test data for iris dataset

--- examples/scikit_learn.py
+++ examples/scikit_learn.py
@@ -1,4 +1,5 @@
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 iris = datasets.load_iris()
 
@@ -7,5 +8,9 @@ iris = datasets.load_iris()
 features = iris.data
 labels = iris.target
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=.5)
+
+print(features_train)
+
 # print(features)
-print(labels)
+# print(labels)
