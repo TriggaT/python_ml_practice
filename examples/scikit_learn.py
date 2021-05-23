744fc3ee5db3293971646879401614afdfdc7e4e
Trayshawn <traytwebb14@yahoo.com>
2021-05-22 19:37:29 -0500

    imported saccuracy_score from sklearn metrics into scikit_learn file

--- examples/scikit_learn.py
+++ examples/scikit_learn.py
@@ -1,6 +1,7 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 iris = datasets.load_iris()
 
